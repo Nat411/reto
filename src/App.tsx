bdb483e797ae22465e73be37c1333cecd8a3af1a
Nat <naalvarezh@unal.edu.co>
2024-04-16 16:38:36 -0500

    feat: Use of hashrouter for urls

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import './App.css'
-import { Routes, Route,BrowserRouter } from 'react-router-dom'
+import { Routes, Route, HashRouter, BrowserRouter } from 'react-router-dom'
 import Home from './components/home'
 import Details from './components/details'
+import Header from './components/header'
 
 function App() {
   return (
 
-  <BrowserRouter>
-  <div>
-    <Routes>
-      <Route path="/" element = {<Home />} />
-      <Route path="/details" element = {<Details />} />
-    </Routes>
-  </div>
-  </BrowserRouter>
+  <HashRouter>
+    {/* <div className='App'>
+          <Header/>
+        </div> */}
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/details" element={<Details />} />
+      </Routes>
+    
+  </HashRouter>
 
   )
 }
