661c6c647d2739959aa4be602b7ddacdb97e4fc0
Nat <naalvarezh@unal.edu.co>
2024-04-17 22:33:47 -0500

    feat: página detalles inicial

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,11 @@ import './App.css'
 import { Routes, Route, HashRouter } from 'react-router-dom'
 import Home from './components/home'
 import Details from './components/details'
-// import Header from './components/header'
 
 function App() {
   return (
 
   <HashRouter>
-    {/* <div className='App'>
-          <Header/>
-        </div> */}
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/details" element={<Details />} />
--- src/components/details/index.tsx
+++ src/components/details/index.tsx
@@ -1,9 +1,33 @@
 import Header from "../header"
+import "./style.css"
+import hotel from "../../assets/hotels/hotel1.webp"
+import hotel2 from "../../assets/hotels/hotel2.webp"
+import hotel3 from "../../assets/hotels/hotel3.webp"
 
 function Details(){
     return (
         <div>
             <Header/>
+            <div className = "details-box">
+                <div className = "details-title-box">
+                    <h3 className = "details-title">Place</h3>  
+                </div>
+                <div className="img-flex">
+                    <img src = {hotel} className="detail-img-big" />
+                    <div className = "img-container">
+                        <img src = {hotel2} className="detail-img-small" />
+                        <img src = {hotel3} className="detail-img-small" />
+                    </div>
+                    <div className = "img-container">
+                        <img src = {hotel2} className="detail-img-small" />
+                        <img src = {hotel3} className="detail-img-small" />
+                    </div>
+                </div>
+                <div className = "details-title-box">
+                    <h3 className = "details-title">Vivienda rentada entero en Calgary, Canadá</h3>  
+                    <p style= {{ margin: 0, color: "var(--subtitle-font)"}}>4 huéspedes  ·  2 habitaciones · 2 camas  ·  2 baños</p>
+                </div>
+            </div>
 
         </div>
     )
